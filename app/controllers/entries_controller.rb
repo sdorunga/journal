88b163737cfb1af7f981d416007eaf000cb1225b
stefan <stefan@stefan-Aspire-5750G.(none)>
2013-03-24 16:37:19 +0000

    change entry controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,14 +1,23 @@
 class EntriesController < ApplicationController
 	def create
+		@entry = Entry.new(params[:entry])
 
+		if @entry.save
+			redirect_to @entry, notice: "successfully created entry"
+		else
+			render action: "new"
+		end
 	end
 
 	def destroy
+		@entry = Entry.find(parans[:id])
+		@entry.destroy
 
+		redirect_to entries_path
 	end
 
 	def edit
-
+		@entry = Entry.find(params[:id])
 	end
 
 	def index
@@ -16,14 +25,21 @@ class EntriesController < ApplicationController
 	end
 
 	def new
-
+		@entry = Entry.new
 	end
 
 	def show
-
+		@entry = Entry.find(params[:id])
 	end
 
 	def update
+		@post = Post.find(params[:id])
 
-	end
+    respond_to do |format|
+      if @post.update_attributes(params[:post])
+        redirect_to @entry, notice: 'Entry was successfully updated.'
+      else
+        render action: "edit"
+      end
+    end
 end
